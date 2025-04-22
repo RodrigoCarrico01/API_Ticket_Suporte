a741406cd58716608bf68f3881916e34a06084ef
RodrigoCarrico01 <mr.rodrigocarrico@gmail.com>
2025-04-22 11:25:52 +0100

    controller moved to a different folder (organizing folders)

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 import http from "node:http"
 
 import { jsonHandler } from "./middlewares/jsonHandler.js"
-import { routeHanlder } from "./middlewares/routeHandler.js"
+import { routeHandler } from "./middlewares/routeHandler.js"
 
 async function listener(request, response){
    await jsonHandler(request, response)
-   routeHanlder(request, response)
+   routeHandler(request, response)
  }
 
 http.createServer(listener).listen(3333)
